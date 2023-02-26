46c85a27f65667c81471f65ab89489dbfaebeffe
nodi-andy <andy@doganmail.de>
2023-02-26 01:54:09 +0100

    smart inserter img

--- src/entity/inserter_long/inserter_long.js
+++ src/entity/inserter_long/inserter_long.js
@@ -1,7 +1,6 @@
 import { Inserter } from "../inserter/inserter.js"
 
 export class InserterLong extends Inserter {
-  static name = 'inserter long'
   static size = [1, 1]
   static type = 'entity'
   static cost = [{ id: "IronPlate", n: 1 }, { id: "Gear", n: 1 }, { id: "HydraulicPiston", n: 1 }]
@@ -12,6 +11,7 @@ export class InserterLong extends Inserter {
     super(pos, data)
     data.pos = pos
     this.setup(undefined, data)
+    this.name = 'InserterLong'
   }
 }
 
--- src/entity/inserter_smart/inserter_smart.js
+++ src/entity/inserter_smart/inserter_smart.js
@@ -1,8 +1,7 @@
-import { Settings } from '../../common.js'
-import { Inventory } from '../../core/inventory.js'
+import { Settings } from "../../common.js"
+import { Inserter } from "../inserter/inserter.js"
 
-export class InserterSmart extends Inventory {
-  static name = 'inserter smart'
+export class InserterSmart extends Inserter {
   static size = [1, 1]
   static type = 'entity'
   static cost = [{ id: "Inserter", n: 2 }, { id: "Gear", n: 1 }, { id: "HydraulicPiston", n: 1 }]
@@ -12,12 +11,31 @@ export class InserterSmart extends Inventory {
     super(pos, data)
     data.pos = pos
     this.setup(undefined, data)
+    this.name = 'InserterSmart'
+  }
+
+  draw (ctx, ent) {
+    ctx.drawImage(InserterSmart.platform, 0, 0, InserterSmart.size[0] * Settings.tileSize, InserterSmart.size[1] * Settings.tileSize, 0, 0, InserterSmart.size[0] * Settings.tileSize, InserterSmart.size[1] * Settings.tileSize)
+  }
+
+  drawItems (ctx) {
+    ctx.save()
+
+    if (this.pos) {
+      ctx.translate(Settings.tileSize * 0.5, Settings.tileSize * 0.5)
+      ctx.rotate(this.armPos * Math.PI / 32)
+      ctx.drawImage(InserterSmart.hand, 0, 0, 64, 64, -48, -15, 64, 64)
+      if (this.isHandFull && this.stack?.INV[0]?.id) {
+        ctx.scale(0.5, 0.5)
+        ctx.drawImage(classDBi[this.stack.INV[0].id].img, -96, -24)
+        ctx.scale(2, 2)
+      }
+    }
+    ctx.restore()
   }
 }
 
-if (typeof Image !== 'undefined') {
-  InserterSmart.platform = new Image(64, 64)
-  InserterSmart.platform.src = './' + InserterSmart.type + '/inserter_smart/inserter_platform.png'
-  InserterSmart.hand = new Image(64, 64)
-  InserterSmart.hand.src = './' + InserterSmart.type + '/inserter_smart/inserter_smart_hand.png'
-}
+InserterSmart.platform = new Image(64, 64)
+InserterSmart.platform.src = './' + InserterSmart.type + '/inserter_smart/inserter_platform.png'
+InserterSmart.hand = new Image(64, 64)
+InserterSmart.hand.src = './' + InserterSmart.type + '/inserter_smart/inserter_smart_hand.png'
